6405422010e21109c8f0ad1f1206db6220b4b5b3
Cel3stt <mariannecelest@gmail.com>
2024-11-15 06:27:23 +0800

    Working on sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,18 @@
 
 import './App.css'
 import Login from './app/Login'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Dashboard from './app/dashboard/Dashboard'
-import Layout from './Layout'
+import { Dashboard } from './app/Dashboard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <BrowserRouter>
-     <Layout>
           <Routes>
               <Route path='/adminlogin' element={<Login/>}/>
               <Route path='/dashboard' element={<Dashboard/>}/>
           </Routes>
-   </Layout>
     </BrowserRouter>
   
   )
