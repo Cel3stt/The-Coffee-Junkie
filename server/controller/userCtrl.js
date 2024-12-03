5f56757dffae184aaade4de9c6a0dfb36b80e0ae
Cel3stt <mariannecelest@gmail.com>
2024-12-03 23:30:05 +0800

    Working with login

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -1,88 +1,18 @@
-const mongoose = require('mongoose'); // Erase if already required
+const User = require("../models/userModels")
 
-// Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true,
-       
-    },
-    lastname:{
-        type:String,
-        required:true,
-      
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-});
-
-//Export the model
-// module.exports = mongoose.model('User', userSchema);
-// // Add Google OAuth fields to schema
-// userSchema.add({
-//   googleId: {
-//     type: String,
-//     unique: true,
-//     sparse: true
-//   },
-//   googleDisplayName: String,
-//   googleEmail: {
-//     type: String,
-//     unique: true,
-//     sparse: true
-//   },
-//   googleProfilePhoto: String
-// });
-
-// // Google OAuth login method
-// userSchema.statics.findOrCreateGoogleUser = async function(profile) {
-//   try {
-//     // Check if user exists
-//     let user = await this.findOne({ googleId: profile.id });
-    
-//     if (user) {
-//       return user;
-//     }
-
-//     // Check if email already exists
-//     user = await this.findOne({ email: profile.emails[0].value });
-    
-//     if (user) {
-//       // Link Google account to existing user
-//       user.googleId = profile.id;
-//       user.googleDisplayName = profile.displayName;
-//       user.googleProfilePhoto = profile.photos[0].value;
-//       await user.save();
-//       return user;
-//     }
-
-//     // Create new user
-//     const newUser = await this.create({
-//       firstname: profile.name.givenName,
-//       lastname: profile.name.familyName,
-//       email: profile.emails[0].value,
-//       googleId: profile.id,
-//       googleDisplayName: profile.displayName,
-//       googleEmail: profile.emails[0].value,
-//       googleProfilePhoto: profile.photos[0].value,
-//       // Set a random password since it's required but not used for OAuth
-//       password: Math.random().toString(36).slice(-8)
-//     });
-
-//     return newUser;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+const createUser = async (req,res) => {
+    const email = req.body.email
+    const findUser = await User.find({email:email});
+        if(!findUser){
+            //create new user
+            const newUser = User.create(req.body)
+            res.json(newUser)
+        } else{
+            //user already exist
+            res.json({
+                msg: "User Already Exists",
+                success: false,
+            })
+        }
+}
+module.exports = {createUser}
--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config()
 const PORT = process.env.PORT || 3000;
+const authRouter = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
 dbConnect()
-
-app.use('/', (req,res) => {
-    res.send("hello from server side")
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/api/user', authRouter)
 app.listen(PORT, () => {
     console.log(`Running at port ${PORT}`)
 })
